d83dffc04175f85ca7e08a2d7433a8cab135a197
Luca Kohn <lkohn@c1r6s3.42luxembourg.lu>
2025-02-07 18:06:17 +0100

    school42

--- Common_Core/test_print_next_line/get_next_line.c
+++ Common_Core/test_print_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lkohn <lkohn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:43:32 by lkohn             #+#    #+#             */
-/*   Updated: 2025/01/31 17:22:43 by lkohn            ###   ########.fr       */
+/*   Updated: 2025/02/07 18:04:50 by lkohn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ char	*ft_free(char *buffer, const char *buf)
 	free(buffer);
 	return (temp);
 }
+
 char	*ft_next(char *buffer)
 {
 	int		i;
@@ -34,14 +35,15 @@ char	*ft_next(char *buffer)
 		free(buffer);
 		return (NULL);
 	}
-	linie = ft_calloc((ft_strlen(buffer) - i + 1), size_of(char));
+	linie = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
 	while (buffer[i])
-		linie[i++] = buffer[i++];
+		linie[i++] = buffer[j++];
 	free(buffer);
 	return (linie);
 }
+
 char	*ft_line(char *buffer)
 {
 	char	*linie;
@@ -52,7 +54,7 @@ char	*ft_line(char *buffer)
 		return (0);
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	linie = ft_calloc(i + 2, size_of(char));
+	linie = ft_calloc(i + 1, sizeof(char));
 	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
 		linie[i] = buffer[i];
@@ -61,34 +63,36 @@ char	*ft_line(char *buffer)
 		linie[i++] = '\n';
 	return (linie);
 }
+
 char	*read_file(const int fd, char *res)
 {
 	char	*buff;
 	int		byte_read;
 
 	if (!res)
-		res = ft_calloc(1, 1);
-	buff = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		res = malloc(1);
+	buff = malloc((BUFFER_SIZE + 1) * sizeof (char));
 	byte_read = 1;
 	while (byte_read > 0)
-		byte_read = read(fd, buff, BUFFER_SIZE);
-	if (byte_read == -1)
-		return (free(res), free(buff), NULL);
-	buff[byte_read] = '\0';
-	res = ft_free(res, buff);
-	if (ft_strchr(buff, '\n'))
 	{
-		break ;
+		byte_read = read(fd, buff, BUFFER_SIZE);
+		if (byte_read == -1)
+			return (free(res), free(buff), NULL);
+		buff[byte_read] = '\0';
+		res = ft_free(res, buff);
+		if (ft_strchr(buff, '\n'))
+			break ;
 	}
 	free(buff);
 	return (res);
 }
-char	*ft_get_next_line(int fd)
-{
-	static char *buffer;
-	char *line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char	*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0) 
 		return (NULL);
 	buffer = read_file(fd, buffer);
 	if (!buffer)
